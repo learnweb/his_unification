79b40be06918ce67ea2d1d297ae86bb781a5ddbb
NinaHerrmann <nina.herrmann@uni-muenster.de>
2020-03-06 15:59:54 +0100

    WIP: processing mform data - security checks and bug fixes

--- handle_customfield_semester.php
+++ handle_customfield_semester.php
@@ -23,6 +23,8 @@
  * @package ?
  */
 
+defined('MOODLE_INTERNAL') || die;
+
 /**
  * Updates or insert the customfield in the 'customfield_data' table.
  * @param $currentsemester int current_semester
@@ -59,103 +61,20 @@ function update_customfield_semester($currentsemester, $courseid){
             ];
             $DB->insert_record('customfield_data', $dataobject);
         }
+    } else {
+        // The key does not exist in the available choices so we will make it a no semester course.
+        $context = $DB->get_record('context', array('contextlevel' => CONTEXT_COURSE, 'instanceid' => $courseid));
+        $currenttimestamp = time();
+        $dataobject = (object) [
+            'fieldid' => $customfield->id,
+            'instanceid' => $courseid,
+            'intvalue' => 1,
+            'value' => 1,
+            'valueformat' => 0,
+            'timecreated' => $currenttimestamp,
+            'timemodified' => $currenttimestamp,
+            'contextid' => $context->id
+        ];
+        $DB->insert_record('customfield_data', $dataobject);
     }
-}
-
-/**
- * @param $current_semester_string
- * @return int
- */
-function calculate_customfieldnumber_from_string($current_semester_string){
-    $numericalrepresentation = 0;
-    switch($current_semester_string) {
-        case 'WS20/21':
-            $numericalrepresentation = 29;
-            break;
-        case 'SS20':
-            $numericalrepresentation = 28;
-            break;
-        case 'WS19/20':
-            $numericalrepresentation = 27;
-            break;
-        case 'SS19':
-            $numericalrepresentation = 26;
-            break;
-        case 'WS18/19':
-            $numericalrepresentation = 25;
-            break;
-        case 'SS18':
-            $numericalrepresentation = 24;
-            break;
-        case 'WS17/18':
-            $numericalrepresentation = 23;
-            break;
-        case 'SS17':
-            $numericalrepresentation = 22;
-            break;
-        case 'WS16/17':
-            $numericalrepresentation = 21;
-            break;
-        case 'SS16':
-            $numericalrepresentation = 20;
-            break;
-        case 'WS15/16':
-            $numericalrepresentation = 19;
-            break;
-        case 'SS15':
-            $numericalrepresentation = 18;
-            break;
-        case 'WS14/15':
-            $numericalrepresentation = 17;
-            break;
-        case 'SS14':
-            $numericalrepresentation = 16;
-            break;
-        case 'WS13/14':
-            $numericalrepresentation = 15;
-            break;
-        case 'SS13':
-            $numericalrepresentation = 14;
-            break;
-        case 'WS12/13':
-            $numericalrepresentation = 13;
-            break;
-        case 'SS12':
-            $numericalrepresentation = 12;
-            break;
-        case 'WS11/12':
-            $numericalrepresentation = 11;
-            break;
-        case 'SS11':
-            $numericalrepresentation = 10;
-            break;
-        case 'WS10/11':
-            $numericalrepresentation = 9;
-            break;
-        case 'SS10':
-            $numericalrepresentation = 8;
-            break;
-        case 'WS09/10':
-            $numericalrepresentation = 7;
-            break;
-        case 'SS09':
-            $numericalrepresentation = 6;
-            break;
-        case 'WS08/09':
-            $numericalrepresentation = 5;
-            break;
-        case 'SS08':
-            $numericalrepresentation = 4;
-            break;
-        case 'WS07/08':
-            $numericalrepresentation = 3;
-            break;
-        case 'SS07':
-            $numericalrepresentation = 2;
-            break;
-        case 'No Semester':
-            $numericalrepresentation = 1;
-            break;
-    }
-    return $numericalrepresentation;
 }
--- request.php
+++ request.php
@@ -5,6 +5,7 @@ require_once(dirname(__FILE__) . '/../../config.php');
 //require_once($CFG->dirroot . '/course/lib.php');
 require_once($CFG->dirroot . '/local/lsf_unification/lib_features.php');
 require_once($CFG->dirroot . '/local/lsf_unification/request_form.php');
+require_once($CFG->dirroot . '/lib/outputlib.php');
 require_once($CFG->dirroot . '/local/lsf_unification/handle_customfield_semester.php');
 
 $veranstid = optional_param('veranstid', null, PARAM_INT);
@@ -132,7 +133,7 @@ function print_res_selection() {
             echo "<b>" . get_string('pre_template', 'local_lsf_unification', $alternative_counter++) . '</b><ul style="list-style-type:none">';
             foreach ($cats as $name => $catfiles) {
                 if (!empty($name)) {
-                    echo '<li style="list-style-image: url(' . $OUTPUT->image_url("t/collapsed")->out(true) . ')" id="reslistselector' . ++$i . '"><a onclick="' . "document.getElementById('reslist" . ($i) . "').style.display=((document.getElementById('reslist" . $i . "').style.display == 'none') ? 'block' : 'none'); document.getElementById('reslistselector" . ($i) . "').style.listStyleImage=((document.getElementById('reslist" . $i . "').style.display == 'none') ? 'url(" . $OUTPUT->pix_url("t/collapsed")->out(true) . ")' : 'url(" . $OUTPUT->pix_url("t/expanded")->out(true) . ")');" . '" style="cursor:default">[<b>' . $name . '</b>]</a></b><ul id="reslist' . $i . '" style="display:none">';
+                    echo '<li style="list-style-image: url(' . $OUTPUT->image_url("t/collapsed")->out(true) . ')" id="reslistselector' . ++$i . '"><a onclick="' . "document.getElementById('reslist" . ($i) . "').style.display=((document.getElementById('reslist" . $i . "').style.display == 'none') ? 'block' : 'none'); document.getElementById('reslistselector" . ($i) . "').style.listStyleImage=((document.getElementById('reslist" . $i . "').style.display == 'none') ? 'url(" . $OUTPUT->image_url("t/collapsed")->out(true) . ")' : 'url(" . $OUTPUT->image_url("t/expanded")->out(true) . ")');" . '" style="cursor:default">[<b>' . $name . '</b>]</a></b><ul id="reslist' . $i . '" style="display:none">';
                 }
                 foreach ($catfiles as $id => $fileinfo) {
                     $lines = explode("\n", trim($fileinfo->info, " \t\r\n"), 2);
@@ -197,7 +198,7 @@ function print_remote_creation() {
 }
 
 function print_coursecreation() {
-    global $CFG, $USER, $OUTPUT, $answer, $teachername, $veranstid, $courseid;
+    global $CFG, $USER, $DB, $OUTPUT, $answer, $teachername, $veranstid, $courseid;
     $editform = new lsf_course_request_form(NULL, array('veranstid' => $veranstid));
     if (!($editform->is_cancelled()) && ($data = $editform->get_data())) {
         // dbenrolment enrolment can only be enabled if it is globally enabled
@@ -219,8 +220,10 @@ function print_coursecreation() {
                 $result['warnings'] .= (send_support_mail($result['course'], $data->category_wish) ? ("\n" . get_string('email_success', 'local_lsf_unification')) : ("\n" . get_string('email_error', 'local_lsf_unification')));
             }
         }
-        // If neccessary update customfield.
-        update_customfield_semester($data->current_semester, $courseid);
+        // Update customfield.
+        if ($DB->get_record('customfield_field', array('name' => 'Semester', 'type' => 'select'))) {
+            update_customfield_semester($data->current_semester, $courseid);
+        }
 
         echo (!empty($result["warnings"])) ? ("<p>" . $OUTPUT->box("<b>" . get_string('warnings', 'local_lsf_unification') . "</b><br>" . "<pre>" . $result["warnings"] . "<pre>") . "</p>") : "";
         print_res_selection($result["course"]->id);
