e0ae7eaff8788ae988409d81fe6e1830d22aadd2
Laur0r <laura-troost@t-online.de>
2024-12-05 15:50:32 +0100

    Updated script for LSF statistic
    This commit is rebased to current main.

--- hup_stats.php
+++ hup_stats.php
@@ -26,12 +26,31 @@ use local_lsf_unification\pg_lite;
 
 defined('MOODLE_INTERNAL') || die();
 
-global $CFG;
+global $CFG, $DB;
 require_once($CFG->dirroot . "/config.php");
 require_once(dirname(__FILE__) . "/class_pg_lite.php");
 require_once(dirname(__FILE__) . ".lib.php");
 require_once(dirname(__FILE__) . "/lib_features.php");
-// Check permissions.
+
+// Konstanten für Fachbereiche
+define("FB1", "FB1: Evangelisch-Theologische Fakultät");
+define("FB2", "FB2: Katholisch-Theologische Fakultät");
+define("FB3", "FB3: Rechtswissenschaftliche Fakultät");
+define("FB4", "FB4: Wirtschaftswissenschaftliche Fakultät");
+define("FB5", "FB5: Medizinische Fakultät");
+define("FB6", "FB6: Erziehungswissenschaften und Sozialwissenschaften");
+define("FB7", "FB7: Psychologie und Sportwissenschaften");
+define("FB8", "FB8: Geschichte und Philosophie");
+define("FB9", "FB9: Philologie");
+define("FB10", "FB10: Mathematik und Informatik");
+define("FB11", "FB11: Physik");
+define("FB12", "FB12: Chemie und Pharmazie");
+define("FB13", "FB13: Biologie");
+define("FB14", "FB14: Geowissenschaften");
+define("FB15", "FB15: Musikhochschule");
+define("FUV", "Fachbereichsunabhängige Veranstaltungen");
+
+/// Check permissions.
 require_login();
 if (!has_capability('moodle/site:config', context_system::instance())) {
     die("no access");
@@ -48,9 +67,9 @@ set_time_limit(30 * 60);
  * @return void
  */
 function create_aggregate() {
-    global $pgdb;
-    pg_query($pgdb->connection, "DROP AGGREGATE IF EXISTS textcat_all(text);");
-    pg_query($pgdb->connection, "CREATE AGGREGATE textcat_all(
+    global $pgDB;
+    pg_query($pgDB->connection, "DROP AGGREGATE textcat_all(text);");
+    pg_query($pgDB->connection, "CREATE AGGREGATE textcat_all(
             basetype    = text,
             sfunc       = textcat,
             stype       = text,
@@ -87,26 +106,23 @@ function get_cat_veranstids_and_count(string $ueids): array {
     global $pgdb;
     $hupstatsveranstcounttable = [];
     if (!empty($ueids)) {
-        $sql = "SELECT veranstaltungsart,
-                       textcat_all(DISTINCT " . HIS_UEBERSCHRIFT . ".veranstid || ',') as veranstids,
+        $sql = "SELECT textcat_all(DISTINCT " . HIS_UEBERSCHRIFT . ".veranstid || ',') as veranstids,
                        COUNT(DISTINCT " . HIS_UEBERSCHRIFT . ".veranstid) as c
                 FROM " . HIS_UEBERSCHRIFT . "
                 JOIN " . HIS_VERANSTALTUNG . "
                 ON " . HIS_UEBERSCHRIFT . ".veranstid = " . HIS_VERANSTALTUNG . ".veranstid
-                WHERE ueid IN (" . $ueids . ")
-                GROUP BY veranstaltungsart;";
+                WHERE ueid IN (" . $ueids . ");";
         $qmain = pg_query($pgdb->connection, $sql);
         while ($hislsftitle = pg_fetch_object($qmain)) {
-            $veranstaltungsart = ["veranstids" => explode(",", $hislsftitle->veranstids), "count" => $hislsftitle->c];
-            $hupstatsveranstcounttable[$hislsftitle->veranstaltungsart] = $veranstaltungsart;
+            $hupstatsveranstcounttable = ["veranstids" => explode(",",$hislsftitle->veranstids), "count" => $hislsftitle->c];
         }
     }
     return $hupstatsveranstcounttable;
 }
 
 
-$pgdb = new pg_lite();
-echo "<p>Verbindung: " . ($pgdb->connect() ? "ja" : "nein") . " (" . $pgdb->connection . ")</p>";
+$pgDB = new pg_lite();
+echo "<p>Verbindung: " . ($pgDB->connect() ? "ja" : "nein") . "</p>";
 create_aggregate();
 
 echo "<p><pre>";
@@ -117,7 +133,6 @@ $toplevelorigins = get_his_toplevel_originids();
 $secondlevelorinins = get_newest_sublevels(implode(", ", $toplevelorigins));
 foreach ($secondlevelorinins as $secondndlevel) {
     // Kategoriekopien herausfinden.
-    $secondndlevel->txt = mb_convert_encoding($secondndlevel->txt, 'UTF-8', 'ISO-8859-1');
     $secondndlevel->copies = $DB->get_records("local_lsf_unification_category", ["origin" => $secondndlevel->origin], null, "ueid");
     foreach ($secondndlevel->copies as $secondlevelcopy) {
         // Semester bestimmen.
@@ -133,38 +148,33 @@ foreach ($secondlevelorinins as $secondndlevel) {
             if (!isset($semstats[$secondlevelcopy->semester])) {
                 $semstats[$secondlevelcopy->semester] = [];
             }
-            if (!isset($semstats[$secondlevelcopy->semester][$secondndlevel->txt])) {
-                $semstats[$secondlevelcopy->semester][$secondndlevel->txt] = [];
-            }
-            foreach ($secondlevelcopy->veranstcount as $typ => $veranstidsandcount) {
-                if (!isset($semstats[$secondlevelcopy->semester][$secondndlevel->txt][$typ])) {
-                    $arr = ["imported" => 0, "existing" => 0, "veranstids" => []];
-                    $semstats[$secondlevelcopy->semester][$secondndlevel->txt][$typ] = $arr;
-                }
-                $veranstids = array_merge(
-                    $semstats[$secondlevelcopy->semester][$secondndlevel->txt][$typ]["veranstids"],
-                    $veranstidsandcount["veranstids"]
-                );
-                $semstats[$secondlevelcopy->semester][$secondndlevel->txt][$typ]["veranstids"] = $veranstids;
-                $semstats[$secondlevelcopy->semester][$secondndlevel->txt][$typ]["existing"] += $veranstidsandcount["count"];
-                // Zaehle bestehende kurse.
-                foreach ($semstats[$secondlevelcopy->semester][$secondndlevel->txt][$typ]["veranstids"] as $veranstid) {
-                    $isimported = $DB->record_exists("course", ["idnumber" => $veranstid]) ? 1 : 0;
-                    $semstats[$secondlevelcopy->semester][$secondndlevel->txt][$typ]["imported"] += $isimported;
-                }
-            }
+            if (!isset($sem_stats[$secondlevelcopy->semester][$secondndlevel->txt])) {
+                $sem_stats[$secondlevelcopy->semester][$secondndlevel->txt] = ["imported" => 0 ,"existing" => 0, "veranstids" => []];
+           }
+           if(!empty($secondlevelcopy->veranstcount) && key_exists("veranstids",
+                    $secondlevelcopy->veranstcount) &&key_exists("count",
+                   $secondlevelcopy->veranstcount)) {
+               $array1 = $sem_stats[$secondlevelcopy->semester][$secondndlevel->txt]["veranstids"];
+               $arrayfilter = array_filter(array_merge($array1,$secondlevelcopy->veranstcount["veranstids"]));
+               $sem_stats[$secondlevelcopy->semester][$secondndlevel->txt]["veranstids"] = $arrayfilter;
+               $sem_stats[$secondlevelcopy->semester][$secondndlevel->txt]["existing"] += $secondlevelcopy->veranstcount["count"];
+               // zaehle bestehende kurse
+               foreach ($sem_stats[$secondlevelcopy->semester][$secondndlevel->txt]["veranstids"] as $veranstid) {
+                   $exists = $DB->record_exists("course", ["idnumber" => $veranstid]) ? 1 : 0;
+                   $sem_stats[$secondlevelcopy->semester][$secondndlevel->txt]["imported"] += $exists;
+               }
+           }
+            $transformedstats = transform_fbs($sem_stats);
         }
     }
 }
 
-// Write CSV.
-echo "Semester;Kategorie;Typ;AnzahlGesamt;AnzahlImportiert\n";
-foreach ($semstats as $sem => $stats) {
+// write CSV
+echo "Semester;Kategorie;AnzahlGesamt;AnzahlImportiert\n";
+foreach ($transformedstats as $sem => $stats) {
     foreach ($stats as $cat => $catstats) {
         if (!empty($catstats)) {
-            foreach ($catstats as $typ => $count) {
-                echo $sem . ";" . $cat . ";" . $typ . ";" . $count["existing"] . ";" . $count["imported"] . "\n";
-            }
+               echo $sem . ";" . $cat . ";" . $catstats["existing"] . ";" . $catstats["imported"] . "\n";
         }
     }
     flush();
@@ -172,5 +182,64 @@ foreach ($semstats as $sem => $stats) {
 
 echo "</pre></p>";
 
-$pgdb->dispose();
-echo "<p>Verbindung geschlossen: " . (($pgdb->connection == null) ? "ja" : "nein") . "</p>";
+$pgDB->dispose();
+echo "<p>Verbindung geschlossen: ".(($pgDB->connection==NULL)?"ja":"nein")."</p>";
+
+function transform_fbs($semesterstats){
+       $transformedstats = [];
+       foreach ($semesterstats as $sem => $stats) {
+           $transformedstats[$sem] = [];
+           foreach ($stats as $cat => $catstats) {
+               switch ($cat){
+                       case 'Evangelische Theologie': $transformedstats[$sem][FB1] = $catstats; break;
+                       case 'Katholische Theologie': $transformedstats[$sem][FB2] = $catstats; break;
+                       case "Rechtswissenschaften": $transformedstats[$sem][FB3] = $catstats; break;
+                       case "Wirtschaftswissenschaften": $transformedstats[$sem][FB4] = $catstats; break;
+                       case "Medizin": $transformedstats[$sem][FB5] = $catstats; break;
+                       case "Sonderpädagogische Fachrichtungen":
+                       case "Erziehungswissenschaft und Sozialwissenschaften":
+                           if(key_exists(FB6,$transformedstats[$sem])){
+                                $transformedstats[$sem][FB6] = merge_stats($transformedstats[$sem][FB6], $catstats);
+                            } else {
+                                $transformedstats[$sem][FB6] = $catstats;
+                            } break;
+                       case "Psychologie und Sportwissenschaft": $transformedstats[$sem][FB7] = $catstats; break;
+                       case "Geschichte/Philosophie": $transformedstats[$sem][FB8] = $catstats; break;
+                       case "Deutsch für Schülerinnen und Schüler mit Zuwanderungsgeschichte (DAZ)":
+                       case "Philologie":
+                           if(key_exists(FB9,$transformedstats[$sem])){
+                                $transformedstats[$sem][FB9] = merge_stats($transformedstats[$sem][FB9], $catstats);
+                            } else {
+                                $transformedstats[$sem][FB9] = $catstats;
+                            } break;
+                       case "Mathematik und Informatik": $transformedstats[$sem][FB10] = $catstats; break;
+                       case "Physik": $transformedstats[$sem][FB11] = $catstats; break;
+                       case "Chemie und Pharmazie": $transformedstats[$sem][FB12] = $catstats; break;
+                       case "Biologie": $transformedstats[$sem][FB13] = $catstats; break;
+                       case "Geowissenschaften": $transformedstats[$sem][FB14] = $catstats; break;
+                       case "Musikhochschule": $transformedstats[$sem][FB15] = $catstats; break;
+                       case "Lehrveranstaltungen":break;
+                       default:
+                           if(!empty($transformedstats[$sem]) && key_exists(FUV,$transformedstats[$sem])){
+                               $transformedstats[$sem][FUV] = merge_stats($transformedstats[$sem][FUV], $catstats);
+                           } else {
+                               $transformedstats[$sem][FUV] = $catstats;
+                           } break;
+                       }
+               }
+               ksort($transformedstats[$sem]);
+       }
+       return $transformedstats;
+}
+
+function merge_stats($catstats, $catstats_add){
+    $output = [];
+    foreach ($catstats as $key => $stats){
+        if(!is_array($stats)) {
+            $output[$key] = $catstats[$key] + $catstats_add[$key];
+        } else {
+            $output[$key] = array_merge($stats, $catstats_add[$key]);
+        }
+    }
+    return $output;
+}
