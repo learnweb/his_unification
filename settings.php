24f0e04496d616c7de4becbb11235a0fdc2aa447
Daniel Meißner <daniel.meissner@uni-muenster.de>
2025-09-25 14:58:56 +0200

    Add Moodle boilerplate comments for settings.php

--- settings.php
+++ settings.php
@@ -1,4 +1,28 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Adds admin settings for local_lsf_unification
+ *
+ * @package     local_lsf_unification
+ * @category    admin
+ * @copyright   2013 Claus Alexander Usener
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die;
 
 if ($hassiteconfig) { // needs this condition or there is error on login page
