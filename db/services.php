506f9ac186fd619525c05c4e5d88cc54cddf225b
Tamaro Walter <tamaro.walter@uni-muenster.de>
2026-03-05 12:03:14 +0100

    add external service to update helptables

--- db/services.php
+++ db/services.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - https://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,20 +12,23 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version info for this plugin.
+ * External functions and service definitions.
  *
  * @package    local_lsf_unification
- * @copyright  2011 Olaf Koehler WWU
- * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  2026 Tamaro Walter
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2025123102;
-$plugin->component = 'local_lsf_unification';
-$plugin->requires  = 2024100700; // Require Moodle 4.5.
-$plugin->supported  = [405, 501];
-$plugin->maturity  = MATURITY_ALPHA;
+$functions = [
+    'local_lsf_unification_update_helptable' => [
+        'classname' => 'local_lsf_unification\external\update_helptable',
+        'methodname' => 'execute',
+        'description' => 'Updates the lsf datatables',
+        'type' => 'write',
+        'ajax' => true,
+    ],
+];
